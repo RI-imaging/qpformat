414e36ecedadf332528929c900bffd5d381500b3
Paul Müller <dev@craban.de>
2019-04-03 22:26:41 +0200

    tests: set correct data time and extract it from zip file

--- tests/test_series_zip_tif_holo.py
+++ tests/test_series_zip_tif_holo.py
@@ -1,5 +1,7 @@
+import os
 import pathlib
 import tempfile
+import time
 import zipfile
 
 import qpformat
@@ -8,9 +10,16 @@ import qpformat
 datapath = pathlib.Path(__file__).parent / "data"
 
 
+DATATIME = 1529500484
+# We need this due to a peculiarity of the zip file format
+UTCOFFSET = time.mktime(time.gmtime(0))
+
+
 def setup_test_zip(num=5):
     _fd, name = tempfile.mkstemp(suffix=".zip", prefix="qpformat_test_holo_")
     tifname = datapath / "single_holo.tif"
+    # We can safely change the time, because git does not synchronize times.
+    os.utime(tifname, times=(DATATIME, DATATIME))
     with zipfile.ZipFile(name, mode="w") as arc:
         for ii in range(num):
             arc.write(tifname, arcname="test_{:04d}.tif".format(ii))
@@ -38,8 +47,8 @@ def test_time():
     path = setup_test_zip(2)
     ds = qpformat.load_data(path)
     ds.get_time(0)
-    # this is the creation date of "singel_holo.tif"
-    assert ds.get_time(0) == 1529500484
+    # this is the creation date of "single_holo.tif"
+    assert ds.get_time(0) == DATATIME - UTCOFFSET
 
     try:
         path.unlink()
